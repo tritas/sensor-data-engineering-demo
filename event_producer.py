c21b78f589280cbf7da5cbcf2879793c2e8cedaf
Aris Tritas <aris@tritas.com>
2022-08-23 20:45:07 +0200

    Clean gitignore, add conda env

--- event_producer.py
+++ event_producer.py
@@ -4,22 +4,23 @@ MQTT Smart temperature Sensor
 
 import time
 
-import paho.mqtt.client as mqtt
+from paho.mqtt.client import Client
 from faker import Faker
 
-# let's connect to the MQTT broker
-MQTT_BROKER_URL    = "mqtt.eclipseprojects.io"
-MQTT_PUBLISH_TOPIC = "temperature"
+if __name__ == "__main__":
+    # let's connect to the MQTT broker
+    MQTT_BROKER_URL = "localhost"
+    MQTT_PUBLISH_TOPIC = "temperature"
 
-mqttc = mqtt.Client()
-mqttc.connect(MQTT_BROKER_URL)
+    mqttc = Client()
+    mqttc.connect(MQTT_BROKER_URL)
 
-# Init faker our fake data provider
-fake = Faker()
+    # Init faker our fake data provider
+    fake = Faker()
 
-# Infinit loop of fake data sent to the Broker
-while True:
-    temperature = fake.random_int(min=0, max=30)
-    mqttc.publish(MQTT_PUBLISH_TOPIC, temperature)
-    print(f"Published new temperature measurement: {temperature}")
-    time.sleep(1)
+    # Infinite loop of fake data sent to the Broker
+    while True:
+        temperature = fake.random_int(min=0, max=30)
+        mqttc.publish(MQTT_PUBLISH_TOPIC, temperature)
+        print(f"Published new temperature measurement: {temperature}")
+        time.sleep(1)
